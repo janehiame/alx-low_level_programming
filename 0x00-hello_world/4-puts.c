e3943f72c065c03094946e7a5f5b46dcc7470524
Jane Eyee Hiame <janehiame@gmail.com>
2023-02-16 15:52:33 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,5 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+int main(void)
+(
+ puts("\"Programming is like building a multilingual puzzle);
+ return (0);
+ )
