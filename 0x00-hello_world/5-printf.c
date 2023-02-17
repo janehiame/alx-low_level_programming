58b94f47e9b6d6c378c2d26806ec504180083ba6
Jane Eyee Hiame <janehiame@gmail.com>
2023-02-16 19:12:44 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf unction
+ * Return:0(successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
