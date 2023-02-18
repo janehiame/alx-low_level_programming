19f04f633436f02d278c9e0b9e40becb27383597
Jane Eyee Hiame <janehiame@gmail.com>
2023-02-17 16:36:02 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		putchar(x);
+	
+	putchar('\n');
+	return (0);
+}
