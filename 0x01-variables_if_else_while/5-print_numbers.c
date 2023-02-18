7f516f918fdcb4c475bc644007f291b6c2087f15
Jane Eyee Hiame <janehiame@gmail.com>
2023-02-17 18:53:27 -0800

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,11 +4,13 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
-	int x;
+	int (x);
 
-	for (x = 0 ; x < 20 ; x++)
-		printf("%d", x)
-	printf("\n")
-	return (0);
+	for (x = 0 ; x < 10 ; x++)
+		printf("%d", x);
+	printf("\n");
+	return(0);
+}
