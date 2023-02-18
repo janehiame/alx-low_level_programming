9251b8330dc19d170b7b6a599c4b7af95255bfdb
Jane Eyee Hiame <janehiame@gmail.com>
2023-02-17 17:41:09 -0800

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,15 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
 	char x;
 
-	for (x = 'a' ; x <= 'z' ; x++)
-	{
+	for (x = 'a' ; x = 'z' ; x++)
+		putchar(x)
+	for (x = 'A' ; x = 'Z' ; x++)
 		putchar(x);
-		if (x == 'c' || x == 'y' || x == 'o')
-			continue;
-		putchar(x);
-	}
-
 	putchar('\n');
-
 	return (0);
 }
